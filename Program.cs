f1b233c47f816e0416e13ff9b5abb204e7cf702a
FelixCrisan <crisanfelix@yahoo.com>
2021-10-23 11:38:37 +0300

    Lab3

--- Program.cs
+++ Program.cs
@@ -2,11 +2,8 @@
 
 namespace MyFirstApp
 {
-    class Program
+    class Program1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       
     }
 }
